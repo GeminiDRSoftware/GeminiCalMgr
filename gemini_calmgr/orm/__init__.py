1050f67266d7dc3523379f86fe30120e57809ec2
Oliver <oly@oberdorf.org>
2021-07-23 15:34:43 -1000

    added back compatibility makkings to FitsStorageDB orm definitions

---
+++ gemini_calmgr/orm/__init__.py
@@ -0,0 +1,6 @@
+
+# for back compatibility for DRAGONS
+import gemini_obs_db.orm.file as file
+import gemini_obs_db.orm.diskfile as diskfile
+import gemini_obs_db.orm.preview as preview
+import gemini_obs_db.orm.header as header
