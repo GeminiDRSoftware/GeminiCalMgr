9a0c4a4accc86fb674bb5a1e922f0df4c6729c0a
Oliver <oly@oberdorf.org>
2021-10-22 15:04:36 -1000

    adding parsing format for datetime

--- tests/gemini_calmgr/cal/test_calibration_gmos.py
+++ tests/gemini_calmgr/cal/test_calibration_gmos.py
@@ -37,10 +37,12 @@ def test_arc(monkeypatch, session):
     # iq = IngestQueueUtil(session, EmptyLogger())
     dummy_ingest_file(raw_arc_file, ['GMOS'], instrument="GMOS-N", program_id="GN-2018B-FT-207",
                       observation_id="GN-2018B-FT-207-29", data_label="GN-2018B-FT-207-29-001",
-                      telescope="Gemini-North", ut_datetime=datetime.strptime('2018-11-13 16:59:10'))
+                      telescope="Gemini-North",
+                      ut_datetime=datetime.strptime('2018-11-13 16:59:10', '%Y-%m-%d %H:%M:%S'))
     dummy_ingest_file(data_file, ['GMOS'], instrument="GMOS-N", program_id="GN-2019B-ENG-51",
                       observation_id="GN-2019B-ENG-51-23", data_label="GN-2019B-ENG-51-23-001",
-                      telescope="Gemini-North", ut_datetime=datetime.strptime('2019-10-02 11:10:09'))
+                      telescope="Gemini-North",
+                      ut_datetime=datetime.strptime('2019-10-02 11:10:09', '%Y-%m-%d %H:%M:%S'))
     # iq.ingest_file(raw_arc_file, "", False, True)
     # iq.ingest_file(data_file, "", False, True)
 
