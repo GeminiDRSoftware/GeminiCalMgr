74f26034b96e5931738cfe864e9e65bf2d7f89fb
Oliver <oly@oberdorf.org>
2021-10-25 08:44:51 -1000

    adding debug prints to see what is going wrong with instrument mocking in astrodata

--- tests/file_helper.py
+++ tests/file_helper.py
@@ -194,8 +194,11 @@ def dummy_ingest_file(filename, tags, instrument=None, program_id=None, observat
                                        requested_cc=requested_cc, requested_iq=requested_iq, requested_wv=requested_wv,
                                        requested_bg=requested_bg, exposure_time=exposure_time, disperser=disperser,
                                        wavelength_band=wavelength_band, detector_binning=detector_binning)
+    print(f"Instrument in dummy_ingest_file: {instrument}")
+    print(f"Astrodata version: {diskfile.ad_object.isntrument}")
     # astrodata.open(diskfile.fullpath())
     dfr = DiskFileReport()  # diskfile, True, True)
     header = Header(diskfile)
+    print(f"Parsed header, it has instrument {header.instrument}")
     name, instClass = instrument_table[header.instrument]
     entry = instClass(header, diskfile.ad_object)
