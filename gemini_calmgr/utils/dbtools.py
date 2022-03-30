f9a2b51a07f810c4987760a06fa14f8c697c913d
Oliver <oly@oberdorf.org>
2022-03-30 09:57:05 -1000

    fixes for command line cal checker tool

--- gemini_calmgr/utils/dbtools.py
+++ gemini_calmgr/utils/dbtools.py
@@ -268,6 +268,7 @@ def add_diskfile_entry(session, fileobj, filename, path, fullpath):
     # Check that it has the correct tags to be identified as a retrievable
     # type of calibration (otherwise it will never be retrieved)
     tags = diskfile.ad_object.tags
+    print(tags)
     for valid_tags in REQUIRED_TAG_DICT.values():
         if tags.issuperset(valid_tags):
             break
